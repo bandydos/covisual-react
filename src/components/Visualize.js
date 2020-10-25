ebd345033bdc58b0416734cd15d677e9a2dab75d
KR44I <38465150+AlexaTack@users.noreply.github.com>
2020-10-25 21:40:22 +0100

    Added bootstrap

--- src/components/Visualize.js
+++ src/components/Visualize.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Visualize extends React.Component {
   constructor(props) {
@@ -21,7 +22,8 @@ class Visualize extends React.Component {
       for (let i = 0; i < jsonresponse.length; i++) {
         records.push({
           'date': jsonresponse[i].date,
-          'deaths': jsonresponse[i].death
+          'deathstoday': jsonresponse[i].deathIncrease,
+          'deathstotal': jsonresponse[i].death
         });
       }
     } else {
@@ -34,7 +36,6 @@ class Visualize extends React.Component {
 
   async componentDidMount() {
     const data = await this.getData();
-    console.log(data);
     this.setState({
       loading: false,
       records: data
@@ -46,16 +47,28 @@ class Visualize extends React.Component {
 
     for (let i = 0; i < this.state.records.length; i++) {
       elems.push(
-        <div>{this.state.records[i].date} : {this.state.records[i].deaths}</div>
+        <tbody key={i}>
+          <tr>
+            <td>{this.state.records[i].date}</td>
+            <td>{this.state.records[i].deathstoday}</td>
+            <td>{this.state.records[i].deathstotal}</td>
+          </tr>
+        </tbody>
       )
     }
 
     return (
-      <div>
-        <div>
-          <h2>Deaths per date</h2>
-        </div>
-        {elems}
+      <div className="container text-center">
+        <table className="table table-striped border border-dark">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Deaths (day)</th>
+              <th>Deaths (total)</th>
+            </tr>
+          </thead>
+          {elems}
+        </table>
       </div>
     )
   }
@@ -74,7 +87,6 @@ class Visualize extends React.Component {
       </div>
     )
   }
-
 }
 
 export default Visualize;
